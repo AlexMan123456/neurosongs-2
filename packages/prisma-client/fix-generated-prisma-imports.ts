e03dca980639b776a2917a4f5a3f8d210d89a3e3
AlexMan123456 <alex.man03888@gmail.com>
2025-08-23 18:45:24 +0100

    Fix all references to generated folder, not just prisma ones

--- packages/prisma-client/fix-generated-prisma-imports.ts
+++ packages/prisma-client/fix-generated-prisma-imports.ts
@@ -11,10 +11,9 @@ function fixImports(filePath: string) {
   let content = fs.readFileSync(filePath, "utf8");
 
   if (filePath.endsWith(".cjs")) {
-    content = content.replace('require("generated/prisma")', 'require("./generated/prisma")');
+    content = content.replace(/require\(['"]generated\/(.*?)['"]\)/g, 'require("./generated/$1")');
   } else {
-    // We need to use a Regex here because index.d.ts uses single-quotes whereas index.js uses double-quotes. Because who needs consistency, am I right?
-    content = content.replace(/from ['"]generated\/prisma['"]/g, 'from "./generated/prisma"');
+    content = content.replace(/from ['"]generated\/(.*?)['"]/g, 'from "./generated/$1"');
   }
 
   fs.writeFileSync(filePath, content);
