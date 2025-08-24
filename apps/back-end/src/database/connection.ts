d7aba70d4832e48238e0d18d46a85e5bd6c7d2f0
AlexMan123456 <alex.man03888@gmail.com>
2025-08-24 02:04:35 +0100

    Add type annotation to make TypeScript happy

--- apps/back-end/src/database/connection.ts
+++ apps/back-end/src/database/connection.ts
@@ -12,6 +12,6 @@ const __dirname = path.dirname(__filename);
 
 dotenv.config({ path: path.join(__dirname, "../../", `.env.${nodeEnv}`) });
 
-const database = new PrismaClient();
+const database: PrismaClient = new PrismaClient();
 
 export default database;
