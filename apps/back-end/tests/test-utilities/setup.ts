b692accaa94d060e6567e2cae4765c36386b9f22
AlexMan123456 <alex.man03888@gmail.com>
2025-08-24 03:17:45 +0100

    Create first user endpoint to get user by ID

--- apps/back-end/tests/test-utilities/setup.ts
+++ apps/back-end/tests/test-utilities/setup.ts
@@ -1,8 +1,13 @@
 import { initialize } from "@neurosongs/prisma-client/fabbrica";
-import { beforeAll } from "vitest";
+import { execa } from "execa";
+import { beforeAll, beforeEach } from "vitest";
 
 import database from "src/database/connection";
 
 beforeAll(() => {
   initialize({ prisma: database });
+});
+
+beforeEach(async () => {
+  await execa("npm", ["run", "reset-database"]);
 });
