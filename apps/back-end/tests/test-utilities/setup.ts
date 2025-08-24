e63726b2bbcc99fc9134f00dfce97140e1f064cc
AlexMan123456 <alex.man03888@gmail.com>
2025-08-24 03:27:52 +0100

    Skip resetting database for now

--- apps/back-end/tests/test-utilities/setup.ts
+++ apps/back-end/tests/test-utilities/setup.ts
@@ -1,13 +1,8 @@
 import { initialize } from "@neurosongs/prisma-client/fabbrica";
-import { execa } from "execa";
-import { beforeAll, beforeEach } from "vitest";
+import { beforeAll } from "vitest";
 
 import database from "src/database/connection";
 
 beforeAll(() => {
   initialize({ prisma: database });
-});
-
-beforeEach(async () => {
-  await execa("npm", ["run", "reset-database"]);
 });
