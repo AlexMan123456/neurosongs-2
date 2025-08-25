70210eb9dcb0364223844cf531a4170cdb9fcb3c
AlexMan123456 <alex.man03888@gmail.com>
2025-08-25 13:12:45 +0100

    Add func-style override to error handlers and validators

--- apps/back-end/src/server/routers/errors.ts
+++ apps/back-end/src/server/routers/errors.ts
@@ -1,6 +1,3 @@
-// All error-handling functions must be arrow functions so that they can be typed as such in Express. As such, we must disable
-// the func-style rule because that expects functions declared with the function keyword.
-/* eslint-disable func-style */
 import type { ErrorRequestHandler } from "express";
 
 import { APIError } from "@alextheman/utility";
--- apps/back-end/src/server/validators/validateUUID.ts
+++ apps/back-end/src/server/validators/validateUUID.ts
@@ -1,6 +1,3 @@
-// All validator functions must be arrow functions so that they can be typed as such in Express. As such, we must disable
-// the func-style rule because that expects functions declared with the function keyword.
-/* eslint-disable func-style */
 import type { RequestParamHandler } from "express";
 
 import { validateUUID as validateUUIDUtility } from "@alextheman/utility";
