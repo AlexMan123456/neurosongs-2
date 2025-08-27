ae3b2f577bbaa698b5616f9e0e9d9bf3e2ec217b
AlexMan123456 <alex.man03888@gmail.com>
2025-08-27 23:22:34 +0100

    Add edge case tests to POST /api/users
    Zod validation comes in handy here!

--- apps/back-end/src/server/routers/errors.ts
+++ apps/back-end/src/server/routers/errors.ts
@@ -1,8 +1,17 @@
 import type { ErrorRequestHandler } from "express";
 
 import { APIError, newEnv } from "@alextheman/utility";
+import { ZodError } from "zod";
 
 const ENV = newEnv(process.env.NODE_ENV);
+
+export const zodErrors: ErrorRequestHandler = (error, _request, response, next) => {
+  if (error instanceof ZodError) {
+    response.status(400).send({ error: JSON.parse(error.message) });
+    return;
+  }
+  next(error);
+};
 
 export const customErrors: ErrorRequestHandler = (error, _request, response, next) => {
   if (error instanceof APIError) {
--- apps/back-end/tests/server/users.test.ts
+++ apps/back-end/tests/server/users.test.ts
@@ -1,4 +1,5 @@
 import type { APIUser, PublicUser, User } from "@neurosongs/prisma-client/types";
+import type { ZodError } from "zod";
 
 import { randomUUID } from "crypto";
 
@@ -85,6 +86,78 @@ describe("/api/users", () => {
       const database = getPrismaClient();
       const postedUser = parseUser(await database.user.findUnique({ where: { id: userId } }));
       expect(postedUser).toMatchObject(user);
+    });
+    test("201: Allow optional properties to be left out", async () => {
+      const user = parseUserToPost({
+        username: "alextheman",
+        artistName: "Alex The Man",
+        profilePicture: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+        dateOfBirth: new Date("2003-07-16T00:00:00.000"),
+        email: "alex@neurosongs.com",
+      });
+
+      const {
+        body: { userId },
+      } = await request(app).post("/api/users").send(user).expect(201);
+
+      const database = getPrismaClient();
+      const postedUser = parseUser(await database.user.findUnique({ where: { id: userId } }));
+      expect(postedUser).toMatchObject(user);
+    });
+    test("400: Do not allow memberSince on request body", async () => {
+      const user = {
+        username: "alextheman",
+        artistName: "Alex The Man",
+        description: "I am an artist on Neurosongs",
+        profilePicture: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+        dateOfBirth: new Date("2003-07-16T00:00:00.000"),
+        email: "alex@neurosongs.com",
+        memberSince: new Date(),
+      };
+
+      const {
+        body: { error },
+      } = await request(app).post("/api/users").send(user).expect(400);
+      error.forEach((errorItem: ZodError["issues"][number]) => {
+        expect(errorItem.code).toBe("unrecognized_keys");
+        expect(errorItem.message).toBe('Unrecognized key: "memberSince"');
+      });
+    });
+    test("400: Error on extra properties", async () => {
+      const user = {
+        username: "alextheman",
+        artistName: "Alex The Man",
+        description: "I am an artist on Neurosongs",
+        profilePicture: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+        dateOfBirth: new Date("2003-07-16T00:00:00.000"),
+        email: "alex@neurosongs.com",
+        extraKey: "Extra property",
+      };
+
+      const {
+        body: { error },
+      } = await request(app).post("/api/users").send(user).expect(400);
+      error.forEach((errorItem: ZodError["issues"][number]) => {
+        expect(errorItem.code).toBe("unrecognized_keys");
+        expect(errorItem.message).toBe('Unrecognized key: "extraKey"');
+      });
+    });
+    test("400: Do not allow mandatory properties to be left out", async () => {
+      const user = {
+        artistName: "Alex The Man",
+        description: "I am an artist on Neurosongs",
+        profilePicture: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+        dateOfBirth: new Date("2003-07-16T00:00:00.000"),
+      };
+
+      const {
+        body: { error },
+      } = await request(app).post("/api/users").send(user).expect(400);
+
+      error.forEach((errorItem: ZodError["issues"][number]) => {
+        expect(errorItem.code).toBe("invalid_type");
+        expect(errorItem.message).toContain("received undefined");
+      });
     });
   });
 });
