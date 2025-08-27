2362251d699f0aea0cbc7d843015aa256eeaea63
AlexMan123456 <alex.man03888@gmail.com>
2025-08-27 23:20:40 +0100

    Allow profile picture to be optional

--- packages/prisma-client/src/types.ts
+++ packages/prisma-client/src/types.ts
@@ -35,6 +35,7 @@ const userToPostSchema = UserInputSchema.omit({
   serial: true,
   memberSince: true,
   dateOfBirth: true,
+  profilePicture: true,
 }).extend({
   dateOfBirth: z.preprocess((value) => {
     if (typeof value === "string" || typeof value === "number") {
@@ -42,6 +43,7 @@ const userToPostSchema = UserInputSchema.omit({
     }
     return value;
   }, z.date()),
+  profilePicture: z.url().optional(),
 });
 
 export type UserToPost = z.infer<typeof userToPostSchema>;
