5f46f20d3015d76d76a2211d646ac27cd30b2323
AlexMan123456 <alex.man03888@gmail.com>
2025-08-27 01:56:20 +0100

    Format and lint the types file
    This file is one worth linting since it contains a fair amount of custom TypeScript. Other files do not need this since they only really export generated stuff.
    
    Note that no-relative-imports must be disabled so that the build can compile correctly.

--- packages/prisma-client/src/types.ts
+++ packages/prisma-client/src/types.ts
@@ -1,24 +1,27 @@
-import { UserModelSchema } from "./generated/types/schemas";
 import z from "zod";
 
+import { UserModelSchema } from "./generated/types/schemas";
+
 const publicUserSchema = UserModelSchema.omit({
-    serial: true,
-    email: true,
-    dateOfBirth: true
+  serial: true,
+  email: true,
+  dateOfBirth: true,
 });
-export type User = z.infer<typeof UserModelSchema>
+export type User = z.infer<typeof UserModelSchema>;
 export type PublicUser = z.infer<typeof publicUserSchema>;
-export function parsePublicUser(data: unknown){
-    return publicUserSchema.parse(data);
+export function parsePublicUser(data: unknown) {
+  return publicUserSchema.parse(data);
 }
 
-const apiUserSchema = publicUserSchema.omit({
-    memberSince: true
-}).extend({
-    memberSince: z.string()
-})
+const apiUserSchema = publicUserSchema
+  .omit({
+    memberSince: true,
+  })
+  .extend({
+    memberSince: z.string(),
+  });
 
-export type APIUser = z.infer<typeof apiUserSchema>
-export function parseAPIUser(data: unknown){
-    return apiUserSchema.parse(data);
+export type APIUser = z.infer<typeof apiUserSchema>;
+export function parseAPIUser(data: unknown) {
+  return apiUserSchema.parse(data);
 }
