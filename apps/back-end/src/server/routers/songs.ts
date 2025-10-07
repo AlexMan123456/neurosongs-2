be10f383d86dede0eb3c184c7c045d1bedb60b20
AlexMan123456 <alex.man03888@gmail.com>
2025-10-07 23:14:54 +0100

    Add endpoint to get all songs, including pagination

--- apps/back-end/src/server/routers/songs.ts
+++ apps/back-end/src/server/routers/songs.ts
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { selectSongById } from "src/server/models/songs";
+import { selectSongById, selectSongs } from "src/server/models/songs";
 
 const songsRouter = express.Router();
 
@@ -8,6 +8,15 @@ songsRouter.route("/:songId").get<{ songId: string }>(async (request, response, 
   try {
     const song = await selectSongById(request.params.songId);
     response.status(200).send({ song });
+  } catch (error) {
+    next(error);
+  }
+});
+
+songsRouter.route("/").get(async (_request, response, next) => {
+  try {
+    const songsResponse = await selectSongs();
+    response.status(200).send(songsResponse);
   } catch (error) {
     next(error);
   }
--- apps/back-end/tests/server/songs.test.ts
+++ apps/back-end/tests/server/songs.test.ts
@@ -1,34 +1,27 @@
+import type { PublicSong } from "@neurosongs/types";
+
 import { randomUUID } from "crypto";
 
+import { fillArray, omitProperties } from "@alextheman/utility";
 import { parsePublicSong } from "@neurosongs/types";
 import request from "supertest";
-import { songFactory } from "tests/test-utilities/dataFactory";
+import { songFactory, userFactory } from "tests/test-utilities/dataFactory";
 import { describe, expect, test } from "vitest";
 
-import getPrismaClient from "src/database/client";
 import app from "src/server/app";
 
 describe("/api/songs/:songId", () => {
   describe("GET", () => {
     test("200: Responds with the given song", async () => {
-      const factorySong = await songFactory.create();
+      const factoryUser = await userFactory.create();
+      const factorySong = await songFactory.create({ artist: { connect: { id: factoryUser.id } } });
       const {
         body: { song: apiSong },
       } = await request(app).get(`/api/songs/${factorySong.id}`).expect(200);
 
       const validatedAPISong = parsePublicSong(apiSong);
-      expect(validatedAPISong).toMatchObject(factorySong);
 
-      const database = getPrismaClient();
-      const factoryUser = await database.user.findUnique({
-        where: {
-          id: factorySong.userId,
-        },
-      });
-
-      if (!factoryUser) {
-        throw new Error("NO_USER");
-      }
+      expect(factorySong).toMatchObject(omitProperties(validatedAPISong, "artistName"));
       expect(validatedAPISong.userId).toBe(factoryUser.id);
       expect(validatedAPISong.artistName).toBe(factoryUser.artistName);
     });
@@ -45,6 +38,64 @@ describe("/api/songs/:songId", () => {
         body: { error },
       } = await request(app).get(`/api/songs/${missingId}`).expect(404);
       expect(error.message).toBe("SONG_NOT_FOUND");
+    });
+  });
+});
+
+describe("/api/songs", () => {
+  describe("GET", () => {
+    test("200: Responds with an array of all songs sorted by most recent", async () => {
+      const factoryUser = await userFactory.create();
+      const factorySongs = await fillArray(async () => {
+        return await songFactory.create({ artist: { connect: { id: factoryUser.id } } });
+      }, 10);
+
+      factorySongs.sort((first, second) => {
+        return first.serial - second.serial;
+      });
+
+      const {
+        body: { songs: apiSongs },
+      }: { body: { songs: PublicSong[] } } = await request(app).get("/api/songs").expect(200);
+
+      expect(apiSongs.length).toBe(10);
+      expect(factorySongs).toMatchObject(
+        apiSongs.map((apiSong) => {
+          const validatedAPISong = parsePublicSong(apiSong);
+          expect(validatedAPISong.artistName).toBe(factoryUser.artistName);
+          expect(validatedAPISong.userId).toBe(factoryUser.id);
+          return omitProperties(parsePublicSong(apiSong), "artistName");
+        }),
+      );
+    });
+    test("200: Gets the first 50 if there are more than 50 users", async () => {
+      const factoryUser = await userFactory.create();
+      const factorySongs = await fillArray(async () => {
+        return await songFactory.create({ artist: { connect: { id: factoryUser.id } } });
+      }, 70);
+
+      factorySongs.sort((first, second) => {
+        return first.serial - second.serial;
+      });
+
+      const {
+        body: { songs: apiSongs, totalSongs, limit, pageNumber, totalPages },
+      } = await request(app).get(`/api/songs`).expect(200);
+
+      expect(totalSongs).toBe(70);
+      expect(limit).toBe(50);
+      expect(pageNumber).toBe(1);
+      expect(totalPages).toBe(2);
+
+      expect(apiSongs.length).toBe(50);
+      expect(factorySongs.slice(0, 50)).toMatchObject(
+        apiSongs.map((apiSong: PublicSong) => {
+          const validatedAPISong = parsePublicSong(apiSong);
+          expect(validatedAPISong.artistName).toBe(factoryUser.artistName);
+          expect(validatedAPISong.userId).toBe(factoryUser.id);
+          return omitProperties(parsePublicSong(apiSong), "artistName");
+        }),
+      );
     });
   });
 });
