84887051d07b157a1fdae633a9e4aded042eda21
AlexMan123456 <alex.man03888@gmail.com>
2025-10-10 00:58:20 +0100

    Add dev data fixtures

---
+++ apps/back-end/src/database/fixtures/index.ts
@@ -0,0 +1,3 @@
+export { default as albumFixtures } from "src/database/fixtures/albums.json";
+export { default as songsFixtures } from "src/database/fixtures/songs.json";
+export { default as usersFixtures } from "src/database/fixtures/users.json";
