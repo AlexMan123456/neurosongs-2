b0432a85229b9cbdc949361fa881c4c4d89a0469
AlexMan123456 <alex.man03888@gmail.com>
2025-10-15 01:20:04 +0100

    Allow for custom API error mapping for formatError, along with an error function for general errors.
    Although this makes the formatError file look very monstrous (especially the formatAPIError function...), it comes with the benefit of being easily extendable in the future, since we can just add to the defaultAPIErrors in the future if we need to do so, and the function takes care of the rest.

--- apps/front-end/src/utility/formatError.ts
+++ apps/front-end/src/utility/formatError.ts
@@ -1,28 +1,89 @@
+import type { HTTPErrorCodes } from "@alextheman/utility";
+
 import { APIError } from "@alextheman/utility";
 import axios from "axios";
 
-function formatAPIError(error: APIError): string {
-  if (error.status === 404) {
-    return (
-      {
-        USER_NOT_FOUND: "Could not find user.",
-        SONG_NOT_FOUND: "Could not find song.",
-        ALBUM_NOT_FOUND: "Could not find album.",
-      }[error.message] ?? "The thing you are trying to look for does not exist."
-    );
+export type APIErrorMap = Partial<
+  Record<HTTPErrorCodes | string, string | Record<string, string | ((error: APIError) => string)>>
+>;
+
+export const defaultAPIErrors: APIErrorMap = {
+  400: {
+    INVALID_UUID: "The ID type you are trying to send is invalid. Please try again.",
+    default: "Error in data being sent. Please try again.",
+  },
+  401: "You do not have permission to do this.",
+  403: "You do not have permission to do this.",
+  404: {
+    USER_NOT_FOUND: "Could not find user.",
+    SONG_NOT_FOUND: "Could not find song.",
+    ALBUM_NOT_FOUND: "Could not find album.",
+    default: "The thing you are trying to look for does not exist.",
+  },
+  418: "Enjoy your tea!",
+  500: "An unknown error has occurred. Please try again later.",
+};
+
+function formatAPIError(error: APIError, apiErrorMap?: APIErrorMap): string {
+  const allErrors: APIErrorMap = !apiErrorMap ? { ...defaultAPIErrors } : {};
+  if (apiErrorMap) {
+    for (const key in defaultAPIErrors) {
+      if (typeof apiErrorMap[key] === "string" && typeof defaultAPIErrors[key] === "string") {
+        allErrors[key] = apiErrorMap[key];
+      } else if (
+        typeof apiErrorMap[key] === "object" &&
+        typeof defaultAPIErrors[key] === "object"
+      ) {
+        allErrors[key] = { ...defaultAPIErrors[key], ...apiErrorMap[key] };
+      } else if (
+        typeof apiErrorMap[key] === "string" &&
+        typeof defaultAPIErrors[key] === "object"
+      ) {
+        allErrors[key] = { ...defaultAPIErrors[key], default: apiErrorMap[key] };
+      } else if (
+        typeof apiErrorMap[key] === "object" &&
+        typeof defaultAPIErrors[key] === "string"
+      ) {
+        if (!Object.keys(apiErrorMap[key]).includes("default")) {
+          allErrors[key] = { ...apiErrorMap[key], default: defaultAPIErrors[key] };
+        } else {
+          allErrors[key] = { ...apiErrorMap[key] };
+        }
+      }
+    }
   }
-  if (error.status === 401 || error.status === 403) {
-    return "You do not have permission to do this.";
+
+  if (allErrors[error.status]) {
+    const statusError = allErrors[error.status];
+    if (typeof statusError === "string") {
+      return statusError;
+    } else if (typeof statusError === "object") {
+      const errorFromMessage = statusError[error.message] ?? statusError.default;
+      if (typeof errorFromMessage === "function") {
+        return errorFromMessage(error);
+      } else if (typeof errorFromMessage === "string") {
+        return errorFromMessage;
+      }
+    }
   }
-  return "Error with API connection. Please try again later.";
+
+  return "Error with API. Please try again later.";
 }
 
-function formatError(error: unknown): string {
+function formatError(
+  error: unknown,
+  apiErrorMap?: APIErrorMap,
+  errorFunction?: (error: unknown) => string,
+): string {
   if (axios.isAxiosError(error)) {
     if (APIError.check(error.response?.data.error)) {
-      return formatAPIError(error.response.data.error);
+      return formatAPIError(error.response.data.error, apiErrorMap);
     }
     return error.message;
+  }
+
+  if (errorFunction) {
+    return errorFunction(error);
   }
   return "An unknown error has occured. Please try again later.";
 }
