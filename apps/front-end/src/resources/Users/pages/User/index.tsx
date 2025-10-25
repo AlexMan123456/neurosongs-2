7049d081d9b723a226d14d71eaddc5d08c16e200
AlexMan123456 <alex.man03888@gmail.com>
2025-10-25 21:21:12 +0100

    Use base loader on user page
    I don't think LoaderProvider is needed here just yet since we're not passing the user down into separate components or loading the user in multiple places. I think, it's probably best to just use the standard Loader where possible and only opt into LoaderProvider if needed. It can be easy to mess up with LoaderProvider as there are more moving parts.

--- apps/front-end/src/resources/Users/pages/User/index.tsx
+++ apps/front-end/src/resources/Users/pages/User/index.tsx
@@ -1,6 +1,4 @@
-import type { PublicUser } from "@neurosongs/types";
-
-import { LoaderData, LoaderError, useHash } from "@alextheman/components";
+import { useHash } from "@alextheman/components";
 import Avatar from "@mui/material/Avatar";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
@@ -10,7 +8,7 @@ import Tab from "@mui/material/Tab";
 import Tabs from "@mui/material/Tabs";
 import { useParams } from "react-router-dom";
 
-import LoaderProvider from "src/components/LoaderProvider";
+import Loader from "src/components/Loader";
 import neurosongsNote from "src/images/Neurosongs_note.png";
 import UserAlbums from "src/resources/Users/pages/User/UserAlbums";
 import UserSongs from "src/resources/Users/pages/User/UserSongs";
@@ -20,8 +18,6 @@ type TabState = "songs" | "albums";
 
 function UserProfile() {
   const { userId } = useParams<{ userId: string }>();
-  // userId should never be undefined since I know this component will only ever be called from the Router under the /users/:userId endpoint,
-  // but this check still needs to be there to satisfy TypeScript.
   if (userId === undefined) {
     throw new Error("USER_ID_PARAMETER_NOT_FOUND");
   }
@@ -29,40 +25,37 @@ function UserProfile() {
   const [tab, setTab] = useHash<TabState>("songs");
 
   return (
-    <LoaderProvider data={user} isLoading={isPending} error={error}>
-      <main>
-        <LoaderError />
-        <Card>
-          <LoaderData<PublicUser>>
-            {(user) => {
-              return (
-                <>
-                  <CardMedia>
-                    <Avatar
-                      src={neurosongsNote}
-                      alt={`${user.artistName}'s profile picture`}
-                      sx={{ width: 56, height: 56, margin: 2 }}
-                    />
-                  </CardMedia>
-                  <CardHeader title={user.artistName} />
-                  <CardContent>{user.description}</CardContent>
-                </>
-              );
-            }}
-          </LoaderData>
-          <Tabs
-            value={tab}
-            onChange={(_, value) => {
-              setTab(value as TabState);
-            }}
-          >
-            <Tab label="Songs" value="songs" />
-            <Tab label="Albums" value="albums" />
-          </Tabs>
-          {{ songs: <UserSongs userId={userId} />, albums: <UserAlbums userId={userId} /> }[tab]}
-        </Card>
-      </main>
-    </LoaderProvider>
+    <main>
+      <Card>
+        <Loader data={user} isLoading={isPending} error={error}>
+          {(user) => {
+            return (
+              <>
+                <CardMedia>
+                  <Avatar
+                    src={neurosongsNote}
+                    alt={`${user.artistName}'s profile picture`}
+                    sx={{ width: 56, height: 56, margin: 2 }}
+                  />
+                </CardMedia>
+                <CardHeader title={user.artistName} />
+                <CardContent>{user.description}</CardContent>
+              </>
+            );
+          }}
+        </Loader>
+        <Tabs
+          value={tab}
+          onChange={(_, value) => {
+            setTab(value as TabState);
+          }}
+        >
+          <Tab label="Songs" value="songs" />
+          <Tab label="Albums" value="albums" />
+        </Tabs>
+        {{ songs: <UserSongs userId={userId} />, albums: <UserAlbums userId={userId} /> }[tab]}
+      </Card>
+    </main>
   );
 }
 
