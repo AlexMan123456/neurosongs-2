c2badf583b67e53109ac7bfae9b649deeb9c7f2e
AlexMan123456 <alex.man03888@gmail.com>
2025-10-26 23:26:24 +0000

    Make the default property optional in apiErrorMap if key is a default HTTPErrorCode
    Since the defaultAPIErrors account for all the HTTPErrorCodes, the default property when passed through elsewhere can just merge with the defaults and therefore not require its own default property. It's only if the error code has not been accounted for that we need the default present.

--- apps/front-end/src/errors/formatError.ts
+++ apps/front-end/src/errors/formatError.ts
@@ -1,37 +1,62 @@
-import type { APIErrorMap } from "src/errors/defaultAPIErrors";
+import type { HTTPErrorCodes } from "@alextheman/utility";
+import type { APIErrorMap, MappingWithRequiredDefault } from "src/errors/defaultAPIErrors";
 
-import { APIError, removeDuplicates } from "@alextheman/utility";
+import { APIError, parseIntStrict, removeDuplicates } from "@alextheman/utility";
 import axios from "axios";
 
-import defaultAPIErrors from "src/errors/defaultAPIErrors";
+import defaultAPIErrorsImport from "src/errors/defaultAPIErrors";
 
 function formatAPIError(error: APIError, apiErrorMap?: APIErrorMap): string {
-  const allErrors: APIErrorMap = !apiErrorMap ? { ...defaultAPIErrors } : {};
+  // I know for a fact that defaultAPIErrors includes a default property, so we can assign it this type.
+  const defaultAPIErrors: Partial<Record<string | HTTPErrorCodes, MappingWithRequiredDefault>> = {
+    ...defaultAPIErrorsImport,
+  };
+  const allErrors: APIErrorMap = !apiErrorMap ? { ...defaultAPIErrorsImport } : {};
   if (apiErrorMap) {
-    const allErrorCodes = removeDuplicates([
+    const allErrorCodes: (string | HTTPErrorCodes)[] = removeDuplicates([
       ...Object.keys(defaultAPIErrors),
       ...Object.keys(apiErrorMap),
     ]);
     for (const code of allErrorCodes) {
-      if (
-        // If both the default message and given message both strings, the provided message takes priority.
-        (typeof apiErrorMap[code] === "string" && typeof defaultAPIErrors[code] === "string") ||
-        // Or if the given message exists but the default doesn't, we also use the given message.
-        (apiErrorMap[code] && !defaultAPIErrors[code])
-      ) {
+      // Ensure the HTTP Error code is a number when stringified (this will error if not)
+      parseIntStrict(code.toString());
+      // If both the default message and given message both strings, the provided message takes priority.
+      if (typeof apiErrorMap[code] === "string" && typeof defaultAPIErrors[code] === "string") {
         allErrors[code] = apiErrorMap[code];
-      } else if (
+      } else if (apiErrorMap[code] && !defaultAPIErrors[code]) {
+        // Provide a default default if default not provided.
+        if (typeof apiErrorMap[code] === "object" && !apiErrorMap[code].default) {
+          allErrors[code] = {
+            ...apiErrorMap[code],
+            default: "An internal server error has occurred. Please try again later.",
+          };
+        }
+        // If default provided, go with that.
+        else if (typeof apiErrorMap[code] === "object" && apiErrorMap[code].default) {
+          allErrors[code] = apiErrorMap[code] as MappingWithRequiredDefault;
+        }
+        // If it's a string, the error mapping can handle strings alone.
+        else if (typeof apiErrorMap[code] === "string") {
+          allErrors[code] = apiErrorMap[code];
+        }
         // If they're both objects, we use a combination of both, always letting the provided ones take priority.
+      } else if (
         typeof apiErrorMap[code] === "object" &&
         typeof defaultAPIErrors[code] === "object"
       ) {
-        allErrors[code] = { ...defaultAPIErrors[code], ...apiErrorMap[code] };
+        allErrors[code] = {
+          ...defaultAPIErrors[code],
+          ...apiErrorMap[code],
+        };
       } else if (
         // If provided is string but default is object, use the rest of the default errors but make the default fallback the provided string.
         typeof apiErrorMap[code] === "string" &&
         typeof defaultAPIErrors[code] === "object"
       ) {
-        allErrors[code] = { ...defaultAPIErrors[code], default: apiErrorMap[code] };
+        allErrors[code] = {
+          ...defaultAPIErrors[code],
+          default: apiErrorMap[code],
+        };
       } else if (
         // If provided is object but default is string, use the whole provided mapping,
         // but if a default fallback is not provided, use the default error as the fallback.
@@ -39,9 +64,13 @@ function formatAPIError(error: APIError, apiErrorMap?: APIErrorMap): string {
         typeof defaultAPIErrors[code] === "string"
       ) {
         if (!Object.keys(apiErrorMap[code]).includes("default")) {
-          allErrors[code] = { ...apiErrorMap[code], default: defaultAPIErrors[code] };
+          allErrors[code] = {
+            ...apiErrorMap[code],
+            default: defaultAPIErrors[code],
+          };
+          // Just assign allErrors[code] to be the apiErrorMap[code] here, since we've already checked to see if default exists previously.
         } else {
-          allErrors[code] = { ...apiErrorMap[code] };
+          allErrors[code] = { ...apiErrorMap[code] } as MappingWithRequiredDefault;
         }
       }
     }
@@ -61,7 +90,7 @@ function formatAPIError(error: APIError, apiErrorMap?: APIErrorMap): string {
     }
   }
 
-  return "An internal server error has occured. Please try again later.";
+  return "An internal server error has occurred. Please try again later.";
 }
 
 function formatError(
@@ -87,8 +116,11 @@ function formatError(
       return errorFunction(error);
     }
     return "An unknown error has occured. Please try again later.";
-  } catch {
-    return "An unknown error has occured. Please try again later.";
+  } catch (error) {
+    if (error instanceof TypeError && error.message === "INTEGER_PARSING_ERROR") {
+      console.error("One or more keys in provided apiErrorMap is not an integer.");
+    }
+    return "An unknown error has occurred. Please try again later.";
   }
 }
 
