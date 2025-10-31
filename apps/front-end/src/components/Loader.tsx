980347101d1b7848806db48f9ca581ae2c496a2f
AlexMan123456 <alex.man03888@gmail.com>
2025-10-31 00:39:47 +0000

    Use the actual LoaderDataProps types to type LoaderProps
    This is also what I do in @alextheman/components now, applied here too so that we can intersect the LoaderProviderProps from Neurosongs instead of the components one (since that has more Neurosongs-specific error handling).

--- apps/front-end/src/components/Loader.tsx
+++ apps/front-end/src/components/Loader.tsx
@@ -1,5 +1,4 @@
-import type { DisallowUndefined } from "@alextheman/utility";
-import type { ReactNode } from "react";
+import type { LoaderDataProps } from "@alextheman/components";
 import type { LoaderProviderProps } from "src/components/LoaderProvider";
 
 // eslint-disable-next-line no-restricted-imports
@@ -7,10 +6,8 @@ import { Loader as AlexLoader } from "@alextheman/components";
 
 import ErrorMessage from "src/components/ErrorMessage";
 
-export type LoaderProps<T> = Omit<LoaderProviderProps<T>, "children"> & {
-  children: ReactNode | ((data: DisallowUndefined<T>) => ReactNode);
-  onUndefined?: () => ReactNode | void;
-};
+export type LoaderProps<T> = Omit<LoaderProviderProps<T>, "children"> &
+  Omit<LoaderDataProps<T>, "showOnError">;
 
 function Loader<T>({ children, apiErrorMap, errorFunction, ...props }: LoaderProps<T>) {
   return (
