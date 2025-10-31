67385bd8d7f39b58202d5088f9f2868603e7ecb3
AlexMan123456 <alex.man03888@gmail.com>
2025-10-31 01:28:09 +0000

    Add UserSongs page
    It's so neat now because the page component itself only really needs a LoaderComponent along with a conditional to decide whether to display one of the existing SongsTable or SongsList components. We don't even need to redefine the same list and table twice, which is really nice.

--- apps/front-end/src/resources/Users/pages/User/UserSongs.tsx
+++ apps/front-end/src/resources/Users/pages/User/UserSongs.tsx
@@ -1,13 +1,25 @@
+import { useScreenSize } from "@alextheman/components";
+import CardContent from "@mui/material/CardContent";
+
+import LoaderProvider from "src/components/LoaderProvider";
+import SongsList from "src/resources/Songs/components/SongsList";
+import SongsTable from "src/resources/Songs/components/SongsTable";
+import { useUserSongsQuery } from "src/resources/Users/queries";
+
 interface UserSongsProps {
   userId: string;
 }
 
 function UserSongs({ userId }: UserSongsProps) {
+  const { data: songs, isPending, error } = useUserSongsQuery(userId);
+  const { isLargeScreen } = useScreenSize();
+
   return (
-    <>
-      <p>Coming soon!</p>
-      <p>{userId}</p>
-    </>
+    <section>
+      <LoaderProvider data={songs} isLoading={isPending} error={error}>
+        <CardContent>{isLargeScreen ? <SongsTable /> : <SongsList />}</CardContent>
+      </LoaderProvider>
+    </section>
   );
 }
 
